e8bc87d30d6f21e3a0f8c9113befd7256e817451
brendan <brendan.linn@gmail.com>
2010-03-30 00:49:31 +0000

    deleted some no longer necessary source files

--- src/awesomest_bug_ever.cpp
+++
@@ -1,9 +0,0 @@
-/* More amusing than the syntax error (which is fairly obvious) is the non-
- * obvious behavior it causes. If we subsequently perform a blocking read
- * from fd, it won't return an error, but rather will appear to block forever.
- * This of course is because fd is now probably 0, which is probably standard
- * input, which of course is going to block on a read! */
-if (fd = open(path.c_str(), O_RDONLY) ==-1) {
-  _LOG_DEBUG("open %s: %m", path.c_str());
-  return NULL;
- }
