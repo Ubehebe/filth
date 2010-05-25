3f34d37360ac833f60d0aa67f11de73a251c620a
Brendan Linn <brendan.linn@gmail.com>
2010-05-25 08:27:34 -0400

    updated binaries. ugh

--- src/HTTP_shell.cpp
+++ src/HTTP_shell.cpp
@@ -10,7 +10,7 @@ int main()
       wait(NULL);
       sleep(60); // For TCP time_wait
     } else {
-      char *const _argv[] = { "./filth", "-m/home/brendan", "-ilo", "-p82", NULL };
+      char *const _argv[] = { "./filth", "-m/home/brendan", NULL };
       char *const _envp[] = { NULL };
       execve("./filth", _argv, _envp);
     }
